ea07d232ac4bda4cacf070627786c6cc8f541c24
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-04 00:07:52 +0530

    Implemented API Gateway

---
+++ 28Nov/apigateway-quizapp/src/main/java/com/microservicesapi/apigateway_quizapp/ApigatewayQuizappApplication.java
@@ -0,0 +1,13 @@
+package com.microservicesapi.apigateway_quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayQuizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayQuizappApplication.class, args);
+	}
+
+}
---
+++ 28Nov/apigateway-quizapp/src/test/java/com/microservicesapi/apigateway_quizapp/ApigatewayQuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesapi.apigateway_quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayQuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
