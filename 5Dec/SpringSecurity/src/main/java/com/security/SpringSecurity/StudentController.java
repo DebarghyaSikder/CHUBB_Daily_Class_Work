0795408e9a662ecd762c464eb4f3b689962a6bb2
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-11 01:50:41 +0530

    continued

--- 5Dec/SpringSecurity/src/main/java/com/security/SpringSecurity/StudentController.java
+++ 5Dec/SpringSecurity/src/main/java/com/security/SpringSecurity/StudentController.java
@@ -2,8 +2,13 @@ package com.security.SpringSecurity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class StudentController {
 	private List<Student> students=new ArrayList<>(List.of(
@@ -15,6 +20,19 @@ public class StudentController {
 	public List<Student> getStudents(){
 		return students;
 		
+	}
+	
+	// Need to generate a csrf token
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+		
+	}
+	
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) {
+		students.add(student);
+		return student;
 	}
 
 }
